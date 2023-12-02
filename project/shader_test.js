9a55e02c80c52b5340fa1b2d7cd9f3c335ab3155
Daniel Alejandro Melo Nuvan <dmelo@unal.edu.co>
2023-12-01 21:59:52 -0500

    Ading brightness control with shaders

--- project/shader_test.js
+++ project/shader_test.js
@@ -1,8 +1,9 @@
-let kernel_shader, image, kernel;
+let kernel_shader, brightness_shader, image, kernel, brightness;
 
 function preload() {
   image = loadImage('./assets/images/cave.jpeg');
   kernel_shader = loadShader('./assets/shaders/kernel.vert', './assets/shaders/kernel.frag');
+  brightness_shader = loadShader('./assets/shaders/kernel.vert', './assets/shaders/brightness.frag');
 }
 
 function setup() {
@@ -12,6 +13,7 @@ function setup() {
   image.resize(600, 600);
   image.loadPixels();
 
+  brightness = 1;
   kernel = [
     0, 0, 0, 0, 0,
     0, -2, 0, -2, 0,
@@ -22,14 +24,29 @@ function setup() {
 }
 
 function draw() {
-  kernel_shader.setUniform('uTexture', image);
-  kernel_shader.setUniform('uKernel', kernel);
-  kernel_shader.setUniform('uStepSize', [1 / width, 1 / height]);
-  kernel_shader.setUniform('uDistance', 1);
-  shader(kernel_shader);
+  // kernel_shader.setUniform('uTexture', image);
+  // kernel_shader.setUniform('uKernel', kernel);
+  // kernel_shader.setUniform('uStepSize', [1 / width, 1 / height]);
+  // kernel_shader.setUniform('uDistance', 1);
+  // shader(kernel_shader);
+
+  brightness_shader.setUniform('uTexture', image);
+  brightness_shader.setUniform('uBrightness', brightness);
+  shader(brightness_shader);
 
   push();
   translate(-width / 2, -height / 2);
   rect(0, 0, width, height);
   pop();
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    brightness -= 0.1;
+    console.log(brightness);
+  }
+  if (keyCode === RIGHT_ARROW) {
+    brightness += 0.1;
+    console.log(brightness);
+  }
 }
